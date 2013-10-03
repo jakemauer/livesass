bfdd1161bdd180cc499b673252309c4122d6ddb3
Jake Mauer <jake@jakemauer.com>
2013-10-03 03:23:00 +0100

    First pass at writing out a tempfile

--- livesass.rb
+++ livesass.rb
@@ -22,13 +22,24 @@ post '/compile-sass' do
   sass = params[:sass]
 
   begin
-    template = default + "body{ color: red;}" + base
+    template = default + params[:sass] + base
     sass_engine = Sass::Engine.new(template, :syntax => :scss)
-    sass_engine.render
+    save_file(sass_engine.render)
   rescue Sass::SyntaxError => e
     status 200
     e.to_s
   end if sass
+end
+
+def save_file(contents)
+  output = Tempfile.open("css")
+  output.write(contents)
+  output.close
+  digest = Digest::MD5.file(output).hexdigest
+  filename = "css_#{digest}.css"
+  File.rename(output, filename)
+  output.unlink
+  filename
 end
 
 # render :scss, template, options, locals
